3b8951869909321d0d2db408bd53ebd9564e3d86
Cong <1712304@student.hcmus.edu.vn>
2020-07-06 08:29:23 +0700

    Update reverse list

--- Day18-ReverseLinkedList.java
+++ Day18-ReverseLinkedList.java
@@ -19,3 +19,26 @@ class Solution {
 
     }
 }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur=head, pre= null, post= null;
+        while(cur!=null){
+            post= cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=post;
+            
+        }
+        return pre;
+    }
+}
